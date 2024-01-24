0e2d766ff355ed6c366216caf69d5157d84354c7
jaerbi <jaerbi42@gmail.com>
2024-01-25 00:32:35 +0200

    Application Initialization remove

--- projects/cend-mat-setup-wizard/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of cend-mat-setup-wizard
- */
-
-export * from './lib/cend-mat-setup-wizard.service';
-export * from './lib/cend-mat-setup-wizard.component';
-export * from './lib/cend-mat-setup-wizard.module';
