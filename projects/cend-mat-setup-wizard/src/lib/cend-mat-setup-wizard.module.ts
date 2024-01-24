371d380408ed2148e5e28bb2c80b5d14022833ba
jaerbi <jaerbi42@gmail.com>
2024-01-25 00:42:54 +0200

    Application Initialization 1

---
+++ projects/cend-mat-setup-wizard/src/lib/cend-mat-setup-wizard.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CendMatSetupWizardComponent } from './cend-mat-setup-wizard.component';
+
+
+
+@NgModule({
+  declarations: [
+    CendMatSetupWizardComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    CendMatSetupWizardComponent
+  ]
+})
+export class CendMatSetupWizardModule { }
